c14826d764ec9e81e7923b2ee8ab952d6c5179f7
sebabouche <sebastien@sifnos.io>
2023-05-01 10:28:18 +0200

    Initial commit

---
+++ src/pages/test.js
@@ -0,0 +1,74 @@
+import React from "react"
+import Layout from "@theme/Layout"
+
+export default function Test() {
+  return (
+    <Layout title="Hello" description="Hello React Page">
+      <div>
+        <h1>TestComponent</h1>
+        <p>
+          Tantum autem cuique tribuendum, primum quantum ipse efficere possis,
+          deinde etiam quantum ille quem diligas atque adiuves, sustinere. Non
+          enim neque tu possis, quamvis excellas, omnes tuos ad honores
+          amplissimos perducere, ut Scipio P. Rupilium potuit consulem efficere,
+          fratrem eius L. non potuit. Quod si etiam possis quidvis deferre ad
+          alterum, videndum est tamen, quid ille possit sustinere.
+        </p>
+        <p>
+          Tantum autem cuique tribuendum, primum quantum ipse efficere possis,
+          deinde etiam quantum ille quem diligas atque adiuves, sustinere. Non
+          enim neque tu possis, quamvis excellas, omnes tuos ad honores
+          amplissimos perducere, ut Scipio P. Rupilium potuit consulem efficere,
+          fratrem eius L. non potuit. Quod si etiam possis quidvis deferre ad
+          alterum, videndum est tamen, quid ille possit sustinere.
+        </p>
+        <p>
+          Tantum autem cuique tribuendum, primum quantum ipse efficere possis,
+          deinde etiam quantum ille quem diligas atque adiuves, sustinere. Non
+          enim neque tu possis, quamvis excellas, omnes tuos ad honores
+          amplissimos perducere, ut Scipio P. Rupilium potuit consulem efficere,
+          fratrem eius L. non potuit. Quod si etiam possis quidvis deferre ad
+          alterum, videndum est tamen, quid ille possit sustinere.
+        </p>
+
+        <h2>Titre 2</h2>
+        <p>
+          Tantum autem cuique tribuendum, primum quantum ipse efficere possis,
+          deinde etiam quantum ille quem diligas atque adiuves, sustinere. Non
+          enim neque tu possis, quamvis excellas, omnes tuos ad honores
+          amplissimos perducere, ut Scipio P. Rupilium potuit consulem efficere,
+          fratrem eius L. non potuit. Quod si etiam possis quidvis deferre ad
+          alterum, videndum est tamen, quid ille possit sustinere.
+        </p>
+
+        <p>
+          Tantum autem cuique tribuendum, primum quantum ipse efficere possis,
+          deinde etiam quantum ille quem diligas atque adiuves, sustinere. Non
+          enim neque tu possis, quamvis excellas, omnes tuos ad honores
+          amplissimos perducere, ut Scipio P. Rupilium potuit consulem efficere,
+          fratrem eius L. non potuit. Quod si etiam possis quidvis deferre ad
+          alterum, videndum est tamen, quid ille possit sustinere.
+        </p>
+
+        <h3>Titre 3</h3>
+        <p>
+          Tantum autem cuique tribuendum, primum quantum ipse efficere possis,
+          deinde etiam quantum ille quem diligas atque adiuves, sustinere. Non
+          enim neque tu possis, quamvis excellas, omnes tuos ad honores
+          amplissimos perducere, ut Scipio P. Rupilium potuit consulem efficere,
+          fratrem eius L. non potuit. Quod si etiam possis quidvis deferre ad
+          alterum, videndum est tamen, quid ille possit sustinere.
+        </p>
+
+        <p>
+          Tantum autem cuique tribuendum, primum quantum ipse efficere possis,
+          deinde etiam quantum ille quem diligas atque adiuves, sustinere. Non
+          enim neque tu possis, quamvis excellas, omnes tuos ad honores
+          amplissimos perducere, ut Scipio P. Rupilium potuit consulem efficere,
+          fratrem eius L. non potuit. Quod si etiam possis quidvis deferre ad
+          alterum, videndum est tamen, quid ille possit sustinere.
+        </p>
+      </div>
+    </Layout>
+  )
+}
---
+++ src/pages/testmarkdown.md
@@ -0,0 +1,13 @@
+---
+title: my hello page title
+description: my hello page description
+hide_table_of_contents: true
+---
+
+# Hello
+
+## How are you?
+
+> Markdown test
+
+![Docusaurus logo (alt text)](/img/docusaurus.png)
